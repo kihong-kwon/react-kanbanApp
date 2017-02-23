5258ab9de95c1c0cbd549fe17f98a353d9ac78d7
kkwon <kihong-kwon@pentain.co.jp>
2017-02-23 23:57:45 +0900

    Chapter1

--- app/App.js
+++ app/App.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
-import {render} from 'react-dom';
-import KanbanBoard from '../component/KanbanBoard'
+import { render } from 'react-dom';
+import KanbanBoard from '../component/KanbanBoard';
 
 let cardList = [
-  {
-    id: 1,
-    title: "Read The Book",
-    description: "I should read the whole book",
-    status: "in-progress",
-    task:[]
-  },
-  {
-    id:2,
-    title: "Write some code",
-    description: "Code along with the samples in the book",
-    status: "todo",
-    task:[
-      {
+    {
         id: 1,
-        name: "ContactList Example",
-        done: true
-      },
-      {
+        title: "Read The Book",
+        description: "I should read the whole book",
+        status: "in-progress",
+        task: []
+    },
+    {
         id: 2,
-        name: "Kanban Example",
-        done: false
-      },
-      {
-        id: 3,
-        name: "My Own experiments",
-        done: false
-      }
-    ]
-  }
+        title: "Write some code",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        task: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My Own experiments",
+                done: false
+            }
+        ]
+    }
 ];
 
-render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
+render(<KanbanBoard cards={cardList} />, document.getElementById('root'));
--- component/List.js
+++ component/List.js
@@ -7,7 +7,7 @@ class List extends Component {
             return <Card id={card.id}
                          title={card.title}
                          description={card.description}
-                         tasks={card.tasks} />
+                         tasks={card.task} />;
         });
 
         return (
